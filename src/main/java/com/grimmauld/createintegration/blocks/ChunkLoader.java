08c8ecf3983e91df78d20c8ab6dbd175fe07e168
alexh <alex.horstkoetter@t-online.de>
2021-06-13 21:34:09 +0200

    trying to get mappings to work

--- src/main/java/com/grimmauld/createintegration/blocks/ChunkLoader.java
+++ src/main/java/com/grimmauld/createintegration/blocks/ChunkLoader.java
@@ -6,15 +6,11 @@ import com.simibubi.create.content.contraptions.base.KineticBlock;
 import com.simibubi.create.content.contraptions.components.structureMovement.MovementBehaviour;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
 
 public class ChunkLoader extends KineticBlock {
     public static MovementBehaviour MOVEMENT = new ChunkLoaderMovementBehaviour();
@@ -26,17 +22,10 @@ public class ChunkLoader extends KineticBlock {
     }
 
     @Override
-    // onBlockAdded
-    public void func_196248_b(BlockState stateIn, IWorld worldIn, BlockPos pos, int flags, int count) {
+    public void onPlace(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
         // TODO not sure if correct
         if (!worldIn.isClientSide()) return;
-        ((ServerWorld) worldIn).getCapability(CreateIntegration.CHUNK_LOADING_CAPABILITY, null).ifPresent(cap -> cap.addblock(pos));
-    }
-
-    @Override
-    // onBlockPlacedBy
-    public void func_180633_a(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
-        super.func_180633_a(worldIn, pos, state, placer, stack);
+        worldIn.getCapability(CreateIntegration.CHUNK_LOADING_CAPABILITY, null).ifPresent(cap -> cap.addblock(pos));
     }
 
     /* @Override
